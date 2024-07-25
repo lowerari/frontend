35248c074d094e2d9ab793682c7023bf56a77eeb
Arianne Lowery <alowery15@gmail.com>
2024-07-25 13:19:28 +0200

    just making sure

--- big-project/src/pages/About.jsx
+++ big-project/src/pages/About.jsx
@@ -18,7 +18,6 @@ export default function About() {
                 <h1>Welcome to <span>MyDBTJourney</span></h1>
                 <p>MyDBTJourney is a web application that provides users with a platform to learn Dialectical Behavior Therapy (DBT) skills. DBT is a type of cognitive-behavioral therapy that is used to treat individuals with borderline personality disorder (BPD). It is also used to treat other mental health conditions such as depression, anxiety, and eating disorders.</p>
                 <p>MyDBTJourney is designed to help users learn DBT skills in a structured and interactive way. The application includes a series of lessons and quizzes that cover the four modules of DBT: mindfulness, distress tolerance, emotion regulation, and interpersonal effectiveness.</p>
-                <p>Users can sign up for an account, log in, and track their progress as they complete the lessons and quizzes. The application also provides users with a profile page where they can view their course progress and update their account information.</p>
                 <p>This site is not intended as a replacement for therapy/professional led mental healthcare. All content on this site is adapted from Dr. Marsha Linehan&apos;s <span>DBT Skills Training Manual, Second Edition</span>.</p>
             </div>
             <div className="creator-info">
